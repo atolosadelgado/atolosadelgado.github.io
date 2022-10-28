e64ce3a05c2345b3e7e3c1bf793636e64a5b2111
alvaro <alvaro@atdpc2.dyndns.cern.ch>
2022-10-28 11:54:27 +0200

    conf.py extended

--- source/conf.py
+++ source/conf.py
@@ -30,6 +30,9 @@ release = '0.0'
 
 # -- General configuration ---------------------------------------------------
 
+import sphinx_rtd_theme
+
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -54,7 +57,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -112,3 +116,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 # pygments_style = 'sphinx'
+
+html_context = {
+    'display_github': True,
+    'github_user': 'HEP-FCC',
+    'github_repo': 'fcc-tutorials',
+    'github_version': 'master/',
+}
+
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+    'colon_fence',
+    'html_admonition'
+]
+
+myst_heading_anchors = 3
